60d058255e02cbc2acab590b77b0d5ff7f93f420
Ramatoulaye CAMARA <camara.ramatoulaye>
2023-12-10 18:10:55 +0000

    first project spring

---
+++ src/main/java/sn/ipsl/tulipe/TulipeApplication.java
@@ -0,0 +1,13 @@
+package sn.ipsl.tulipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TulipeApplication.class, args);
+	}
+
+}
